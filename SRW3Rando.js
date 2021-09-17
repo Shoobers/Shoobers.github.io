9b02aa6c531d54b9c0c6ca10cc5ba1ac898bd258
Shoobers <jonkociuba@gmail.com>
2021-09-16 21:38:33 -0400

    Update SRW3Rando.js

--- SRW3Rando.js
+++ SRW3Rando.js
@@ -206,7 +206,7 @@ function randomize(rom, rng, opts) {
 
 	//scenario 1
 	//on scenario start
-	for ( let i = 0; i < 15; i+=1)
+/*	for ( let i = 0; i < 15; i+=1)
 	{
 		rom[scenarioOneEnemy[0][0]+i*8]=pilotList[math.floor( rng() * pilotList.length())];  //Pilots
 		rom[scenarioOneEnemy[0][1]+i*8]=math.floor( rng() * 12);  //level
@@ -245,7 +245,7 @@ function randomize(rom, rng, opts) {
 //		rom[scenarioOneAlly[0][5]+i*3-1]=math.floor(rng()*23); // y position
 		rom[scenarioOneAlly[0][6]+i*3-1]=rom[scenarioOneAlly[0][0]+i*4];
 	}
-	
+*/	
 	
 	
 	
