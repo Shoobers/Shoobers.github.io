eb1bf79ac77341661d6ea9447b3e20135b3a9e11
Shoobers <jonkociuba@gmail.com>
2021-09-18 16:51:45 -0400

    Minor code restructuring to look semi neater, minor bug fixing.

--- SRW3Rando.js
+++ SRW3Rando.js
@@ -1,8 +1,8 @@
 function randomize(rom, rng, opts) {
 
 	enemyRandomize(16, 0, scenarioOneEnemy, pilotList, fullEnemyMechs, rom, rng);  // scenario 1 enemy on map spawn
-	enemyRandomize(16, 1, scenarioOneEnemy, pilotList, fullEnemyMechs, rom, rng);  // scenario 1 reinforcements
-	allyRandomize(10, 0, scenarioOneAlly, pilotList, fullAllyMechs, rom, rng);	// scenario 1 ally spawn
+	enemyRandomize(5, 1, scenarioOneEnemy, pilotList, fullEnemyMechs, rom, rng);  // scenario 1 reinforcements
+	allyRandomize(11, 0, scenarioOneAlly, pilotList, fullAllyMechs, rom, rng);	// scenario 1 ally spawn
 	//scenario 1
 	//on scenario start
 
--- scenLoops.js
+++ scenLoops.js
@@ -1,14 +1,36 @@
+
+function rand ( min, max, rng )
+{
+	let r = rng();
+	r *= max-min+1;
+	return Math.floor(r+min);
+}
+
+function pick_rand ( arr, rng, min=0 )
+{
+	return arr[rand(min, arr.length-1, rng)];
+}
+
+/*
+Index defines where the table ends
+wave indicates how many waves in we are ( 0 is map spawn, 1 is first set of reinforcements, 2 is another set of reinforcements )
+scenEnemy is a two dimensional array with hexvalues, it's two dimensional in case there's more areas that need to be looked at.  This'll probably be the most important for talk triggers and the like when a certain enemy should be talked to etc. 
+pilots is the set of pilots available to this map in an array of hex values
+mechs is the set of mechs available to this map in an array of hex values
+rom is the file
+rng is the seed
+*/
 //enemy spawns
-function enemyRandomize(index, wave, scenEnemy, pilots, mechs, rom, rng)
+function enemyRandomize(index, wave, scenEnemy, pilots, mechs, rom, rng)   
 	{
 		for ( let i = 0; i < index; i+=1)
 		{
-			rom[(scenEnemy[0][wave]+(i*8))]=pilots[Math.floor( rng() * pilots.length)];  //Pilots
-			rom[(scenEnemy[1][wave]+(i*8))]=(Math.floor( rng() * 12)+1);  //level
-			rom[(scenEnemy[2][wave]+(i*8))]=mechs[Math.floor( rng() * mechs.length)]; //mechs
-			rom[(scenEnemy[3][wave]+(i*8))]=(Math.floor( rng() * 5 )+1); // turns until activation
-//			rom[(scenEnemy[4][wave]+(i*8))]=Math.floor( rng() * 23 ); // x position
-//			rom[(scenEnemy[5][wave]+(i*8))]=Math.floor( rng() * 23 ); // y position
+			rom[(scenEnemy[0][wave]+(i*8))]=pick_rand(pilots, rng);  //Pilots
+			rom[(scenEnemy[1][wave]+(i*8))]=rand(1, 12, rng);  //level
+			rom[(scenEnemy[2][wave]+(i*8))]=pick_rand(mechs, rng); //mechs
+			rom[(scenEnemy[3][wave]+(i*8))]=rand(1, 5, rng); // turns until activation
+//			rom[(scenEnemy[4][wave]+(i*8))]=rand(0, 23, rng); // x position
+//			rom[(scenEnemy[5][wave]+(i*8))]=rand(0, 23, rng); // y position
 //			rom[(scenEnemy[6][wave]+(i*8))]=Donotchangeyet;
 		}
 	return rom;
@@ -16,20 +38,27 @@ function enemyRandomize(index, wave, scenEnemy, pilots, mechs, rom, rng)
 
 
 
-
-
+/*
+Index defines where the table ends
+wave indicates how many waves in we are ( 0 is map spawn, 1 is first set of reinforcements, 2 is another set of reinforcements )
+scenEnemy is a two dimensional array with hexvalues, it's two dimensional in case there's more areas that need to be looked at.  This'll probably be the most important for talk triggers and the like when a certain enemy should be talked to etc. 
+pilots is the set of pilots available to this map in an array of hex values
+mechs is the set of mechs available to this map in an array of hex values
+rom is the file
+rng is the seed
+*/
 //ally spawns
 function allyRandomize(index, wave, scenAlly, pilots, mechs, rom, rng)
 {
 	for ( let i = 0; i < index; i+=1)
 	{
-		let v = pilots[Math.floor( rng() * pilotList.length)];
+		let v = pick_rand(pilots, rng);
 		rom[(scenarioOneAlly[0][wave]+(i*4))]=v//pilots
-		if ( i > 0 && i < (index-2) ) {	
+		if ( i > 0 && i < (index-1) ) {	  // -2 is due to Ryoma/Hayato/(Musashi/Benkei position) // These three pilots also have to have their setup dealt with elsewhere due to getter transitioning
 			rom[(scenarioOneAlly[6][wave]+((i-1)*3))]=v;
 		}
-		rom[(scenarioOneAlly[1][wave]+(i*4))]=(Math.floor( rng() * 8)+1); //level
-		rom[(scenarioOneAlly[2][wave]+(i*4))]=mechs[Math.floor( rng() * mechs.length)]; //mechs 
+		rom[(scenarioOneAlly[1][wave]+(i*4))]=rand(1,8, rng); //level
+		rom[(scenarioOneAlly[2][wave]+(i*4))]=pick_rand(mechs, rng); //mechs 
 //		rom[(scenarioOneAlly[3][wave]+(i*4))]=Donotchangeyet;
 	}
 //	rom[scenarioOneAlly[4][0]]=Math.floor(rng()*23); // x position
@@ -38,7 +67,7 @@ function allyRandomize(index, wave, scenAlly, pilots, mechs, rom, rng)
 //	{
 //		rom[(scenarioOneAlly[4][0]+((i-1)*3))]=Math.floor(rng()*23); // x position
 //		rom[(scenarioOneAlly[5][0]+((i-1)*3))]=Math.floor(rng()*23); // y position
-//		rom[(scenarioOneAlly[6][0]+((i-1)*3))]=rom[scenarioOneAlly[0][0]+i*4];
+//		rom[(scenarioOneAlly[6][0]+((i-1)*3))]=rom[scenarioOneAlly[0][0]+i*4];  // I believe I already set it up for the above
 //	}
 	return rom;
 }
