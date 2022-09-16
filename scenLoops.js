90cffbc88fa49eb80d1068be88a7854da7b2131e
Shoobers <jonkociuba@gmail.com>
2022-09-16 19:05:00 -0400

    Update scenLoops.js

--- scenLoops.js
+++ scenLoops.js
@@ -65,8 +65,6 @@ function enemyDespawn ( spoilLog, despawnArray, rom, arrgrid, despawnAddr )
 		{
 			for ( let k = 0; k < despawnAddr[j+1][0]; k+=1 )
 			{
-				alert(rom[despawnAddr[j+1][k]]);
-				alert(rom[despawnArray[i+1]]);
 				rom[despawnAddr[j+1][k]]=rom[despawnArray[i+1]];
 			}
 		}
